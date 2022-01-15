2ac11be4fdbe95aef63cbc4a4980bddedd142bf9
David_Valdaliso_Lopez <dvaldaliso@gmail.com>
2022-01-15 00:39:54 -0500

    optimzando codigo

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -71,6 +71,35 @@ class _SingleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return _CardBackground(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          backgroundColor: this.color,
+          child: Icon(
+            this.icon,
+            size: 35,
+            color: Colors.white,
+          ),
+          radius: 30,
+        ),
+        SizedBox(height: 10),
+        Text(
+          this.text,
+          style: TextStyle(color: this.color, fontSize: 18),
+        )
+      ],
+    ));
+  }
+}
+
+class _CardBackground extends StatelessWidget {
+  final Widget child;
+
+  const _CardBackground({Key? key, required this.child}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15),
       child: ClipRRect(
@@ -82,28 +111,11 @@ class _SingleCard extends StatelessWidget {
             decoration: BoxDecoration(
                 color: Color.fromRGBO(62, 66, 107, 0.7),
                 borderRadius: BorderRadius.circular(20)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: this.color,
-                  child: Icon(
-                    this.icon,
-                    size: 35,
-                    color: Colors.white,
-                  ),
-                  radius: 30,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  this.text,
-                  style: TextStyle(color: this.color, fontSize: 18),
-                )
-              ],
-            ),
+            child: this.child,
           ),
         ),
       ),
     );
+    ;
   }
 }
