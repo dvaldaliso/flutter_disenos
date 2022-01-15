f8af4026466bc09a40168399ae9341ee93f448c0
David_Valdaliso_Lopez <dvaldaliso@gmail.com>
2022-01-14 23:40:22 -0500

    adicionando botones

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:disenos/screens/scroll_desing.dart';
 import 'package:flutter/material.dart';
 
 import 'package:disenos/screens/basic_design.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,38 @@
 import 'package:disenos/widgets/bakcground.dart';
+import 'package:disenos/widgets/card_table.dart';
+import 'package:disenos/widgets/custom_bottom_navigation.dart';
+import 'package:disenos/widgets/page_title.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        //Background
-        Background()
-      ],
-    ));
+      body: Stack(
+        children: [
+          //Background
+          Background(),
+          //Home body
+          _HomeBody(),
+        ],
+      ),
+      bottomNavigationBar: CustomBottomNAvigation(),
+    );
+  }
+}
+
+class _HomeBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          //Titulos
+          PageTitle(),
+          // Card table
+          CardTable()
+        ],
+      ),
+    );
   }
 }
